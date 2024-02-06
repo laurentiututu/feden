ae97cb15aa4a5f83f971bd80947331c2050ef18d
Laurentiu Tutu <tutu.v.laurentiu@gmail.com>
2024-02-06 20:20:10 +0200

    Demo banner

---
+++ src/components/molecules/m-header-top.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="m-header-top">
+    <p>This is a preview mode. Some data may not be available.</p>
+  </div>
+</template>
+<style lang="scss" scoped>
+.m-header-top {
+  width: 100%;
+  height: 2rem;
+  background-color: #f0ad4e;
+  color: #90682f;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid #90682f;
+
+  p {
+    margin: 0;
+    font-weight: 500;
+    font-size: 0.875rem;
+  }
+}
+</style>
